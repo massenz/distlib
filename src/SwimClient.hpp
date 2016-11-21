e99df82eeb4ba00ddff54bde02afe15fcc73d4c5
Marco Massenzio <m.massenzio@gmail.com>
2016-11-20 23:08:37 -0800

    Implementing Ping server

--- src/SwimClient.hpp
+++ src/SwimClient.hpp
@@ -3,12 +3,16 @@
 
 #pragma once
 
+#include <zmq.hpp>
 
 #include "swim.pb.h"
 #include "network.h"
 
 
-#define DEFAULT_TIMEOUT_MSEC    250
+
+// Default items to be reported; see SWIM protocol and `SwimReport` in swim.proto.
+#define MAX_ALLOWED_DEFAULT  256
+
 
 namespace swim {
 
@@ -33,10 +37,31 @@ static Server makeServer(const std::string &hostname, int port, const std::strin
 
 class SwimClient {
 
-  Server server_;
+  static const unsigned long DEFAULT_TIMEOUT_MSEC;
+  static const unsigned int DEFAULT_SOCKET_LINGER_MSEC;
+
+  Server dest_;
   unsigned long timeout_;
   Server self_;
+  unsigned int max_allowed_reports_ = MAX_ALLOWED_DEFAULT;
 
+  /**
+   * Builds the URI for the `dest` server, using the hostname and port.
+   *
+   * @return a string of the form {@literal tcp://hostname:port}
+   */
+  const std::string destinationUri() const {
+    return "tcp://" + dest_.hostname() + ":" + std::to_string(dest_.port());
+  }
+
+  /**
+   * Common functionality to send an envelop to the destination server; adds this host (`self_`)
+   * coordinates and a timestamp.
+   *
+   * @param envelope the message to be sent to the destination (`dest_`) server.
+   * @return the response from the server, is successful; `nullptr` otherwise.
+   */
+  bool postMessage(SwimEnvelope *envelope) const;
 
 public:
 
@@ -45,7 +70,7 @@ public:
    *
    * @param server where to send the message to.
    */
-  SwimClient(const Server &server, int self_port, unsigned long timeout = DEFAULT_TIMEOUT_MSEC);
+  SwimClient(const Server &dest, int self_port, unsigned long timeout = DEFAULT_TIMEOUT_MSEC);
 
   SwimClient(const std::string &hostname, int port, int self_port = 0,
              unsigned long timeout = DEFAULT_TIMEOUT_MSEC) :
@@ -62,7 +87,7 @@ public:
    * to be in an "unhealthy" state (and must do so before the "grace period" expires).
    */
   // TODO: make it return a Future instead
-  void ping();
+  bool ping();
 
   /**
    * Sends a status report on all known nodes' statuses changes.
@@ -76,7 +101,7 @@ public:
   void send(const SwimReport *report);
 
   /**
-   * Requests the server to ping `other` and verify its status.
+   * Requests the `dest` server to ping `other` and verify its status.
    * See the SWIM protocol for more details.
    *
    * @param other the server to be pinged on behalf of this one
@@ -85,10 +110,19 @@ public:
 
   /**
    * Utility method to obtain this server's coordinates.
-   * 
+   *
    * @return this server
    */
   const Server& self() const { return self_; }
+
+  unsigned long timeout() const;
+
+  void setTimeout(unsigned long timeout);
+
+  unsigned int max_allowed_reports() const;
+
+  void setMax_allowed_reports(unsigned int max_allowed_reports_);
+
 };
 
 } // namespace swim
